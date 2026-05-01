7e6e8eb1f8410e31055aebf3f080455c5a962224
iliapetrov-ops <ilia.petrov@e-u.edu.ua>
2026-05-01 04:19:42 +0300

    Update task03.rs

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,15 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+fn staircase(n: usize) {
+    for i in 1..=n {
+        let spaces = n - i;
+        let hashes = i;
+        println!("{}{}", " ".repeat(spaces), "#".repeat(hashes));
+    }
+}
+fn main() {
+    use std::io;
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let n: usize = input.trim().parse().expect("Please enter a number");
+    staircase(n);
+}
